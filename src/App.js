49d819aa0d18594124651ebd32484e5f679517d5
m_emin <mehmeteminbul@gmail.com>
2022-03-31 22:18:20 +0300

    product items added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductItem from "./components/product/productItem";
+import "./App.css";
 function App() {
+  //     okuma     yazma
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then(({ data }) => {
+      console.log("nasılsın", data);
+      setProducts(data);
+    });
+    // const {data} = await axios.get('https://fakestoreapi.com/products')
+    // console.log("naber", data)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((osman) => (
+        <>
+          <ProductItem ahmet={osman} />
+        </>
+      ))}
     </div>
   );
 }
-
 export default App;
